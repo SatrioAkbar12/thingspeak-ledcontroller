a7447b42424a4b9d44c07627f2a269189b7b22f2
SatrioAkbar12 <65120345+SatrioAkbar12@users.noreply.github.com>
2022-11-11 08:46:23 +0700

    add index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>LED ON/OFF</title>
+  </head>
+  <body>
+    <p align="center">
+      <font size="8">
+        <b>LED 1 = &nbsp <a href="https://api.thingspeak.com/update?api_key=0Y8IAGFRL39193C&field1=1">ON</a></b> / <b><a href="https://api.thingspeak.com/update?api_key=0Y8IAGFRL39193C&field1=0">OFF</a></b> <br>
+        <b>LED 2 = &nbsp <a href="https://api.thingspeak.com/update?api_key=0Y8IAGFRL39193C&field2=1">ON </a></b> / <b><a href="https://api.thingspeak.com/update?api_key=0Y8IAGFRL39193C&field2=0">OFF</a></b> <br>
+      </font>
+    </p>
+  </body>
+</html>
